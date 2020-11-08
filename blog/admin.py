ab02bac5b60e6c420eef08b225048d6a9c4b3032
baha <bahabahnur2@gmail.com>
2020-11-08 22:59:32 +0600

    5 lesstions

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
 from django.contrib.admin import register
 
-from .models import Post
+from .models import Post, Tag
 
 @register(Post)
-class CarAdmin(admin.ModelAdmin):
+class PostAdmin(admin.ModelAdmin):
     list_display = (
          'title',
         'body',    )
+@register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = [
+        'title'
+    ]
 
 # Register your models here.
