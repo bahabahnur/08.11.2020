9cd7f7f909b8484246e26b66f6dbb273c9efa703
baha <bahabahnur2@gmail.com>
2020-11-09 12:37:32 +0600

    10 lesstions

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from django.views.generic import View
 from .models import *
 from .utils import *
 from .forms import TagForm, PostForm
-
+from django.urls import reverse
 
 
 def posts_list(request):
@@ -19,28 +19,43 @@ class PostDetail(ObjectDetailMixin, View):
     template = 'blog/post_detail.html'
 
 
-class PostCreate(ObjectCreatelMixin, View):
-    model_form = PostForm
-    template = 'blog/post_create_form.html'
-
-class PostUpdate(ObjectUpdateMixin, View):
-    model = Post
-    form_model = PostForm
-    template = 'blog/post_update_form.html'
-
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
     template = 'blog/tag_detail.html'
+
+
+class PostCreate(ObjectCreatelMixin, View):
+    model_form = PostForm
+    template = 'blog/post_create_form.html'
 
 
 class TagCreate(ObjectCreatelMixin, View):
     model_form = TagForm
     template = 'blog/tag_create.html'
 
+
+class PostUpdate(ObjectUpdateMixin, View):
+    model = Post
+    form_model = PostForm
+    template = 'blog/post_update_form.html'
+
+
 class TagUpdate(ObjectUpdateMixin, View):
     model = Tag
     form_model = TagForm
     template = 'blog/tag_update_form.html'
+
+class TagDelete(ObjectDeleteMixin, View):
+    model = Tag
+    template = 'blog/tag_delete_form.html'
+    redirect_url = 'tags_list_url'
+
+class PostDelete(ObjectDeleteMixin, View):
+    model = Post
+    template = 'blog/post_delete_form.html'
+    redirect_url = 'posts_list_url' 
+ 
+
 
 
 def tags_list(request):
